0aa7dc1c2a30bd14324eb3f0cd8ba5291f2c4a6c
Sailesh Sasikumar <your-email@example.com>
2026-03-12 06:44:12 +0530

    frontend

--- ambientAI/app/main.py
+++ ambientAI/app/main.py
@@ -1,12 +1,29 @@
+# from fastapi import FastAPI
+# from app.api.routes import router
+
+# app = FastAPI(title="Kanad Ambient AI Backend")
+
+# app.include_router(router)
+
+
+# @app.get("/")
+# def health():
+
+#     return {"status": "running"}
+
+
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.api.routes import router
 
-app = FastAPI(title="Kanad Ambient AI Backend")
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],   # or ["http://localhost:5173"]
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(router)
-
-
-@app.get("/")
-def health():
-
-    return {"status": "running"}
